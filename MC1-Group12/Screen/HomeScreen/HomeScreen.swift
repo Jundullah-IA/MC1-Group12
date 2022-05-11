b0b666895d3d84be13fb7bfaea952079615fb314
ditcu-code <work.adityac@gmail.com>
2022-05-11 21:54:24 +0700

    ditcu - state folder structure

---
+++ MC1-Group12/Screen/HomeScreen/HomeScreen.swift
@@ -0,0 +1,22 @@
+//
+//  HomeScreen.swift
+//  MC1-Group12
+//
+//  Created by Aditya Cahyo on 11/05/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        VStack {
+            Text("Hikit").font(.title)
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
