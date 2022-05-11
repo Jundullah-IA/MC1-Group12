b317fd4b11cb34da739405e6b7a49e584dd5e55e
ditcu-code <work.adityac@gmail.com>
2022-05-11 21:20:43 +0700

    ditcu - add gitignore

--- MC1-Group12/ContentView.swift
+++ MC1-Group12/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        Text("Test")
+        VStack{
+            Text("Hello, world!")
+                .padding()
+            Text("Test")
+        }
     }
 }
 
