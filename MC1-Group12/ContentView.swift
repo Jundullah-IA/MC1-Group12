f93d9d3b7e1f22c8f7833a822f47e9920110eb48
jun-ia <jundullah.ilhaq@gmail.com>
2022-05-11 09:27:50 +0700

    Initial Commit

---
+++ MC1-Group12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC1-Group12
+//
+//  Created by Jundullah Ilhaq Aulia on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC1-Group12/MC1_Group12App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_Group12App.swift
+//  MC1-Group12
+//
+//  Created by Jundullah Ilhaq Aulia on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MC1_Group12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
