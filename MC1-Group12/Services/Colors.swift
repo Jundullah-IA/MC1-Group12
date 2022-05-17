2409129ad7dc58a565ea0b341c17a7974ecda749
ditcu-code <work.adityac@gmail.com>
2022-05-17 21:01:40 +0700

    ditcu - create home and mountain list screen

---
+++ MC1-Group12/Services/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  MC1-Group12
+//
+//  Created by Aditya Cahyo on 14/05/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkGreen = Color("dark-green")
+    static let background = Color("bg")
+}
